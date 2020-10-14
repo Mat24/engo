5d143783839226b701cb25ded14711c8d0e7e5aa
josnelihurt.rb <josnelihurt.rb@globant.com>
2020-10-14 16:13:35 -0500

    backlog

--- cmd/main.go
+++ cmd/main.go
@@ -664,6 +664,16 @@ func (p *PauseSystem) Update(dt float32) {
 	}
 }
 
+/*
+1. Refactor, show some order!
+2. Explore collision using engo
+3. Explore background sound
+4. HID on mouse click
+5. Explore animmation
+6. Enemy?
+7. Handle live, game over?
+*/
+
 func main() {
 	opts := engo.RunOptions{
 		Title:      "My Little Adventure",
